31b68e1a733b3d683e1bbb2246dfeef71aa138d8
N-meapp <developersnme@gmail.com>
2026-07-10 15:53:48 +0530

    ashif update

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import StudentExperience from '../Components/StudentsExperience/StudentsExperience'
+
+const HomePage = () => {
+  return (
+    <>
+    <StudentExperience />
+    </>
+  )
+}
+
+export default HomePage
